4f5e519411bf239d14ce609d27c0f3fdda4b8ca8
Patrick Kelly <pjkellysf@yahoo.com>
2015-10-31 10:34:27 -0700

    Update palindromes.py

--- palindromes.py
+++ palindromes.py
@@ -1,24 +1,29 @@
 #!/usr/local/bin/python3
 # Patrick Kelly
-# CS131A
 # October 28, 2015
 
-#A program which finds any palindromes over seven letters long in the dictionary /users/abrick/resources/american-english-insane
+# A program which finds any palindromes over seven letters long in the dictionary
+# /users/abrick/resources/american-english-insane
 
-# Set the content of the file to a variable.
-wrapper = open('/users/abrick/resources/american-english-insane')
-# Assign the minimum length of the palindrome to be found.
+# Set the content of the dictionary file to a file handler.
+fh = open('/users/abrick/resources/american-english-insane')
+# Assign the minimum length of the palindrome to be found and initialize the count to zero.
 minLetters = 7
+count = 0
 
-# Iterate through the contents
-for line in wrapper:
-    # Strip out the line return and assign the result to a variable
+# Iterate through the contents.
+for line in fh:
+    # Strip out the line return and assign the result to a variable.
     word = line.strip()
-    # Compare the forwards and backwards strings
+    # Compare the forwards and backwards strings.
     #   AND
-    # Compare the length to the minimum length
+    # Compare the length of the word to the minimum length.
     if word == word[::-1] and (len(word) > minLetters):
-        #Print out the result
+        # If the word is a palindrome and meets the length requirement, increment the count and print the result.
+        count += 1
         print('The word',word,'is a palindrome with more than',minLetters,'letters.')
         
-wrapper.close()
+# Print the total number of palindromes found in the dictionary that meet the requierements.
+print('The dictionary contains',count,'palindromes with more than',minLetters,'letters.')
+# Close the file handler.
+fh.close()
